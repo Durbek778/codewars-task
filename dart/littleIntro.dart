653906918600aad20246a48f9c5d97224194461a
ilkhomjon2002 <dergasbaev@gmail.com>
2024-03-20 14:02:26 +0900

    intro

---
+++ dart/littleIntro.dart
@@ -0,0 +1,19 @@
+void main(){
+
+    String name = 'Durbek';
+    print("Hi, myname is ${name}");
+
+    int year = 4;
+    print("I am ${year} student");
+
+    String major = 'Computer Science & BBA';
+    print("My major is ${major}");
+
+    int age = 25;
+    print("I am ${age} years old");
+
+    int gpt = 4;
+    print("my CGPA is ${gpt}");
+
+}
+
